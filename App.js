b1fb9fd9381349062591e1ee8bed720ce85cb1cb
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2021-12-04 00:58:37 -0500

    create boilerplate application with navigation drawer

--- App.js
+++ App.js
@@ -1,14 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
+import { createDrawerNavigator } from '@react-navigation/drawer'
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
+const Drawer = createDrawerNavigator()
 
-export default function App() {
+const MyTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: 'rgb(255, 255, 255)'
+  }
+}
+
+export default function App () {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <NavigationContainer theme={MyTheme}>
+      <Drawer.Navigator initialRouteName='HomeScreen'>
+        <Drawer.Screen name='Home' component={HomeScreen} />
+        <Drawer.Screen name='Phone' component={Phone} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  )
+}
+
+function HomeScreen ({ navigation }) {
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+      <Button
+        onPress={() => navigation.navigate('Phone')}
+        title='Go to Phone'
+      />
     </View>
-  );
+  )
+}
+
+function Phone ({ navigation }) {
+  return (
+    <View>
+      <Text>Phone</Text>
+      <Button onPress={() => navigation.goBack()} title='Go back home' />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -16,6 +50,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
