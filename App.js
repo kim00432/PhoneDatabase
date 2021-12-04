20ba137e92f38feaa709b52f1bb80ec7e2710fc1
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2021-12-04 15:42:13 -0500

    add icons in drawer, make AppDrawer a seperate component

--- App.js
+++ App.js
@@ -6,26 +6,58 @@ import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
 import Phone from './screens/Phone'
 import HomeScreen from './screens/HomeScreen'
 const Drawer = createDrawerNavigator()
+import { Ionicons } from '@expo/vector-icons'
 
-const MyTheme = {
+export default function App () {
+  return (
+    <NavigationContainer theme={DrawerTheme}>
+      <AppDrawer />
+    </NavigationContainer>
+  )
+}
+
+function AppDrawer () {
+  return (
+    <Drawer.Navigator initialRouteName='Home'>
+      <Drawer.Screen
+        name='Home'
+        component={HomeScreen}
+        options={{
+          drawerIcon: ({ focused, color, size }) => {
+            if (focused) {
+              return <Ionicons name='md-home' size={size} color={color} />
+            } else {
+              return (
+                <Ionicons name='md-home-outline' size={size} color={color} />
+              )
+            }
+          }
+        }}
+      />
+      <Drawer.Screen
+        name='Details'
+        component={Phone}
+        options={{
+          drawerIcon: ({ focused, color, size }) => {
+            if (focused) {
+              return <Ionicons name='list' size={size} color={color} />
+            } else {
+              return <Ionicons name='list-outline' size={size} color={color} />
+            }
+          }
+        }}
+      />
+    </Drawer.Navigator>
+  )
+}
+
+const DrawerTheme = {
   ...DefaultTheme,
   colors: {
     ...DefaultTheme.colors,
     background: 'rgb(255, 255, 255)'
   }
 }
-
-export default function App () {
-  return (
-    <NavigationContainer theme={MyTheme}>
-      <Drawer.Navigator initialRouteName='HomeScreen'>
-        <Drawer.Screen name='Home' component={HomeScreen} />
-        <Drawer.Screen name='Phone' component={Phone} />
-      </Drawer.Navigator>
-    </NavigationContainer>
-  )
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
