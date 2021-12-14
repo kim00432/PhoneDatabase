4617fde6441274a6c2e163e02a5e09cdfb0c405f
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2021-12-14 14:17:33 -0500

    create useAsyncStorage custom hook

---
+++ hooks/useAsyncStorage.js
@@ -0,0 +1,32 @@
+//custom hook for local storage
+import { useState, useEffect } from 'react'
+
+export default function useAsyncStorage (storeKey) {
+  const [val, setVal] = useState(() => {
+    //
+    //
+    const storedValue = async () => {
+      let storedValue = await AsyncStorage.getItem(storeKey)
+      if (storedValue) {
+        console.log(`Found stored data: ${storedValue}`)
+        return JSON.parse(storedValue)
+      }else{
+        console.log(
+          `No stored data found(${storedValue})`
+        )
+        return
+      }
+    }
+
+    useEffect(() => {
+      try {
+        const jsonValue = JSON.stringify(val)
+        await AsyncStorage.setItem(storeKey, jsonValue)
+      } catch(e) {
+        console.log('saving error')
+      }
+    }, [storeKey, val])
+  
+    return [val, setVal]
+  })
+}
