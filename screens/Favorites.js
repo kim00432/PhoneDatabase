538220d69e9e68670c604d1950663e89bf9ff032
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2021-12-14 14:37:00 -0500

    import PhonesContext

--- screens/Favorites.js
+++ screens/Favorites.js
@@ -2,11 +2,44 @@ import React from 'react'
 import { StyleSheet, Text, View, Button, SafeAreaView } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
 
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
 export default function Favorites ({ navigation }) {
+  const storeData = async value => {
+    try {
+      await AsyncStorage.setItem('yoona-jc', value)
+      console.log('saved string')
+    } catch (e) {
+      // saving error
+      console.log('saving error')
+    }
+  }
+
+  const getData = async () => {
+    try {
+      let value = await AsyncStorage.getItem('yoona-jc')
+      console.log(value)
+    } catch (e) {
+      // read error
+    }
+  }
+
   return (
     <SafeAreaView style={styles.container} edges={['right', 'bottom', 'left']}>
       <View>
         <StatusBar style='auto' />
+        <Button
+          onPress={() => storeData('dgwdthwrthwrt')}
+          title='save string'
+        />
+
+        <Button
+          onPress={() => {
+            getData()
+          }}
+          title='get string'
+        />
+
         <Text>Favorites</Text>
         <Button onPress={() => navigation.goBack()} title='Go back home' />
       </View>
