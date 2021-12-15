5c65115187c41109f1b2cd8c071276285b2dbd3e
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2021-12-14 20:28:59 -0500

    fix synthax issue, hook gt m ethod not in the right place.

--- hooks/useAsyncStorage.js
+++ hooks/useAsyncStorage.js
@@ -4,29 +4,26 @@ import { useState, useEffect } from 'react'
 export default function useAsyncStorage (storeKey) {
   const [val, setVal] = useState(() => {
     //
-    //
     const storedValue = async () => {
       let storedValue = await AsyncStorage.getItem(storeKey)
       if (storedValue) {
         console.log(`Found stored data: ${storedValue}`)
         return JSON.parse(storedValue)
-      }else{
-        console.log(
-          `No stored data found(${storedValue})`
-        )
+      } else {
+        console.log(`No stored data found(${storedValue})`)
         return
       }
     }
-
-    useEffect(() => {
-      try {
-        const jsonValue = JSON.stringify(val)
-        await AsyncStorage.setItem(storeKey, jsonValue)
-      } catch(e) {
-        console.log('saving error')
-      }
-    }, [storeKey, val])
-  
-    return [val, setVal]
   })
+
+  useEffect(async () => {
+    try {
+      const jsonValue = JSON.stringify(val)
+      await AsyncStorage.setItem(storeKey, jsonValue)
+    } catch (e) {
+      console.log('saving error')
+    }
+  }, [val, val])
+
+  return [val, setVal]
 }
