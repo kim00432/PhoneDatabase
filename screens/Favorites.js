fea64330aaf527797e17650eed4995f327f58f6e
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2021-12-14 21:04:14 -0500

    add testing buttons for storage

--- screens/Favorites.js
+++ screens/Favorites.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { StyleSheet, Text, View, Button, SafeAreaView } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
 import { usePhonesDetails } from '../context/PhonesContext'
+import { useEffect, useState } from 'react/cjs/react.development'
 
 export default function Favorites ({ navigation }) {
   const [
@@ -12,8 +13,17 @@ export default function Favorites ({ navigation }) {
     phoneURL,
     setPhoneURL,
     phoneDetails,
-    setPhoneDetails
+    setPhoneDetails,
+    getFavorites,
+    addToFavorites,
+    deleteFromFavorites
   ] = usePhonesDetails()
+
+  const [data, setData] = useState()
+  useEffect(() => {
+    let fetchedData = getFavorites()
+    setData(fetchedData)
+  }, [])
 
   return (
     <SafeAreaView style={styles.container} edges={['right', 'bottom', 'left']}>
@@ -21,6 +31,14 @@ export default function Favorites ({ navigation }) {
         <StatusBar style='auto' />
         <Text>Favorites</Text>
         <Button onPress={() => navigation.goBack()} title='Go back home' />
+      </View>
+      <View>
+        <Text>Testing Storage:</Text>
+        <Button
+          title='save item'
+          onPress={() => addToFavorites({ test: 'test' })}
+        />
+        {data && console.log(data)}
       </View>
     </SafeAreaView>
   )
