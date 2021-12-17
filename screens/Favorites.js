a6608674bc8097597c21e9773636486e67da6e90
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2021-12-16 22:06:08 -0500

    complete styling for Favorites

--- screens/Favorites.js
+++ screens/Favorites.js
@@ -3,14 +3,15 @@ import {
   StyleSheet,
   Text,
   View,
-  Button,
   SafeAreaView,
   FlatList,
   Pressable
 } from 'react-native'
-import { StatusBar } from 'expo-status-bar'
+
 import { usePhonesDetails } from '../context/PhonesContext'
 import { useEffect, useState } from 'react/cjs/react.development'
+
+import { Ionicons } from '@expo/vector-icons'
 
 export default function Favorites ({ navigation }) {
   const [
@@ -34,37 +35,33 @@ export default function Favorites ({ navigation }) {
 
   return (
     <SafeAreaView style={styles.container} edges={['right', 'bottom', 'left']}>
-      <View>
-        <StatusBar style='auto' />
-        <Text>Favorites</Text>
-        <Button onPress={() => navigation.goBack()} title='Go back home' />
-      </View>
-      <View>
-        <Text>Testing Storage:</Text>
-        <Button
-          title='save item'
-          onPress={() =>
-            addToFavorites({
-              brand: 'apple',
-              phone_name: 'iphone 33',
-              detail:
-                'http://api-mobilespecs.azharimm.site/v2/apple_iphone_13_pro_max-11089'
-            })
-          }
-        />
-      </View>
       {data && (
         <FlatList
+          //Title section
           ListHeaderComponent={
-            <View
-              style={{ paddingVertical: 8, backgroundColor: 'grey' }}
-            ></View>
+            <View>
+              <Text
+                style={{
+                  ...styles.header,
+                  marginVertical: 17,
+                  marginHorizontal: 17
+                }}
+              >
+                Favorites
+              </Text>
+              <Text
+                style={{
+                  ...styles.body,
+                  marginHorizontal: 17,
+                  marginBottom: 34
+                }}
+              >
+                You can add favorites by clicking on the heart shaped icon
+                inside the Details page. Long press to remove.
+              </Text>
+            </View>
           }
-          ListFooterComponent={
-            <View
-              style={{ paddingVertical: 8, backgroundColor: 'grey' }}
-            ></View>
-          }
+          //Favorites list
           data={data}
           renderItem={favorite => (
             <Favorite
@@ -78,6 +75,7 @@ export default function Favorites ({ navigation }) {
           keyExtractor={(item, index) => {
             return item + '-' + index
           }}
+          ListFooterComponent={<View style={{ paddingVertical: 8 }}></View>}
         />
       )}
     </SafeAreaView>
@@ -87,26 +85,65 @@ export default function Favorites ({ navigation }) {
 function Favorite ({ brand, phone_name, phoneURL, setPhoneURL, navigation }) {
   return (
     <Pressable
+      style={{ ...styles.favoritesCard, marginHorizontal: 17 }}
       onPress={ev => {
         setPhoneURL(`${phoneURL}`)
         console.log(phoneURL)
         navigation.navigate('Details')
       }}
     >
-      <View>
-        <Text>{brand}</Text>
-        <Text>{phone_name}</Text>
+      <View style={{ display: 'flex', flexDirection: 'row' }}>
+        <Text style={styles.body}>{brand}</Text>
+        <Text style={{ ...styles.title, paddingLeft: 4 }}>{phone_name}</Text>
       </View>
+      <Ionicons name='chevron-forward' size={24} color='#007AFF' />
     </Pressable>
   )
 }
 
 const styles = StyleSheet.create({
+  header: {
+    fontSize: 34,
+    fontWeight: '600',
+    color: '#000'
+  },
+  title: {
+    fontSize: 17,
+    fontWeight: '600',
+    color: '#000'
+  },
+  body: {
+    fontSize: 17,
+    fontWeight: '400',
+    color: '#000'
+  },
+  buttonText: {
+    fontSize: 17,
+    fontWeight: '600',
+    color: '#FFF'
+  },
   container: {
     display: 'flex',
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  favoritesCard: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: 'white',
+    paddingVertical: 20,
+    paddingHorizontal: 17,
+    marginVertical: 8,
+    borderRadius: 17,
+
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 9 },
+    shadowOpacity: 0.14,
+    shadowRadius: 9,
+    elevation: 9
   }
 })
